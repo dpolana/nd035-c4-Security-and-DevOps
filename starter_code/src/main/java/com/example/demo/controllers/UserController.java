fa350a2e9f9c30952f71be20ae004576cf915adb
Délio Polana <deliopolana@gmail.com>
2021-06-14 12:32:43 +0200

    added logging on controller classes

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,7 +23,9 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -49,14 +53,17 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+		log.debug("Verifying password requirements for user "+user.getUsername());
 		if(createUserRequest.getPassword().length() < 7 ||
 			!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword()) ||
 			createUserRequest.getPassword().contains(createUserRequest.getUsername()))
 		{
+			log.error("Unable to create User! Error validating password requirements");
 			return ResponseEntity.badRequest().build();
 		}
 		user.setPassword(encoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
+		log.info("User "+user.getUsername()+" has been successfully created!");
 		return ResponseEntity.ok(user);
 	}
 }
